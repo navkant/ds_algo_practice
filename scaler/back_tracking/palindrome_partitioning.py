23cdf46981147624458125b1b9c7fdf4e7da38dc
navkant <navkant.tyagi9@gmail.com>
2021-07-22 08:01:51 +0530

    added new problems

--- scaler/back_tracking/palindrome_partitioning.py
+++ scaler/back_tracking/palindrome_partitioning.py
@@ -1,20 +1,25 @@
 class Solution:
     # @param A : string
     # @return a list of list of strings
-    def is_palindrome(self, string):
+    def is_palindrome(self, string1):
+        if len(string1) == 1:
+            return True
+        if len(string1) == 0:
+            return False
+
         i = 0
-        n = len(string) - 1
+        n = len(string1) - 1
 
         while True:
             if i == n:
                 break
             if i + 1 == n:
-                if string[i] == string[n]:
+                if string1[i] == string1[n]:
                     return True
                 else:
                     return False
 
-            if string[i] == string[n]:
+            if string1[i] == string1[n]:
                 pass
             else:
                 return False
@@ -22,24 +27,26 @@ class Solution:
             n -= 1
         return True
 
-    def palindrome_partition_recurse(self, index, string, result, subset):
-        if index > len(string):
+    def palindrome_partition_recurse(self, i, string, result, subset):
+        # print(f'i: {i}')
+        if i == len(string):
             # print(subset)
             result.append([x for x in subset])
             return
 
-        if self.is_palindrome(string[:index]):
-            subset.append(string[:index])
-            # print(subset)
-            self.palindrome_partition_recurse(1, string[index:], result, subset)
-            subset.pop()
-
-        self.palindrome_partition_recurse(index+1, string, result, subset)
+        for j in range(i, len(string)):
+            # print(f'string: {string[i:j+1]}')
+            if self.is_palindrome(string[i:j+1]):
+                # print(f'string {string[i:j]} is palindrome')
+                subset.append(string[i:j+1])
+                self.palindrome_partition_recurse(j+1, string, result, subset)
+                subset.pop()
 
     def partition(self, A):
         ans = []
-        self.palindrome_partition_recurse(1, A, ans, [])
-        print(f'ans is {ans}')
+        self.palindrome_partition_recurse(0, A, ans, [])
+        for row in ans:
+            print(row)
 
 
 if __name__ == '__main__':
